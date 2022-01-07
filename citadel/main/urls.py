61eb32dfdba0efc986423b0a336189df1cc1ad00
speedUpDev <70540091+speedUpDev@users.noreply.github.com>
2022-01-07 20:00:03 +0500

    links change

---
+++ citadel/main/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', index, name='Citadel'),
+    path('news/', news, name='news'),
+    path('media/', video, name='media'),
+    path('photos/', photos, name='photos'),
+    path('player/', player, name='player'),
+    path('streams/', streams, name='streams'),
+    path('video/', video, name='video'),
+    path('article/', article, name='article'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('registration/', RegisterUser.as_view(), name='registration'),
+    path('tournaments/', tournaments, name='tournaments'),
+    path('info/', info, name='info'),
+    path('shop/', shop, name='shop'),
+    # path('news/<int:articleid>/', news)
+]
