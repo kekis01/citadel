adea8953ee78e935edc4af296e93c0ea9a6887e7
speedUpDev <70540091+speedUpDev@users.noreply.github.com>
2022-01-08 19:27:56 +0500

    comments_counters

---
+++ citadel/main/static/js/count_comments.js
@@ -0,0 +1,4 @@
+let li = document.querySelectorAll('.comment_example');
+let count = li.length;
+document.getElementById("count").innerHTML = count;
+document.getElementById("count1").innerHTML = count;
